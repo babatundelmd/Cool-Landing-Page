b02f6afde207df1d580f817cbce2f5ce12c46d7e
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-04-13 22:55:04 +0100

    dark theme in motion

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const toggle = document.querySelector( 'input[name=check-box]' );
-console.log("The Script is here")
+const body = document.getElementById( 'body' );
+const container = document.querySelector( '.container' );
+const hone =  document.querySelector('h1, h2')
+
 
 toggle.addEventListener( 'change', ( e ) =>
 {
-    if ( e.target.checked === true ) {
-        console.log( e, "Light Theme")
+    if ( e.target.checked === false ) {
+        body.classList.toggle( 'dark-theme' );
+        container.classList.toggle( 'container-dark' );
+        hone.classList.toggle('font-dark')
+        console.log( e, "Dark Theme")
     } else {
-        console.log(e, "Dark Theme")
+        body.classList.remove( 'dark-theme');
+        container.classList.remove( 'container-dark' );
+        hone.classList.remove('font-dark')
     }
 
 } )
