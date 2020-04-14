3d0d3d177676d475ab91e23a3cb6a9770dfb8025
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-04-14 23:09:13 +0100

    attempt to darken card

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const toggle = document.querySelector( 'input[name=check-box]' );
 const body = document.getElementById( 'body' );
 const container = document.querySelector( '.container' );
-const hone =  document.querySelector('h1, h2')
+const hone = document.querySelector( 'h1' );
+const cards = document.querySelector( 'div.cards' );
+
 
 
 toggle.addEventListener( 'change', ( e ) =>
@@ -9,12 +11,15 @@ toggle.addEventListener( 'change', ( e ) =>
     if ( e.target.checked === false ) {
         body.classList.toggle( 'dark-theme' );
         container.classList.toggle( 'container-dark' );
-        hone.classList.toggle('font-dark')
+        hone.classList.toggle( 'font-dark' );
+        cards.classList.toggle( 'cards-dark' );
         console.log( e, "Dark Theme")
     } else {
         body.classList.remove( 'dark-theme');
         container.classList.remove( 'container-dark' );
-        hone.classList.remove('font-dark')
+        hone.classList.remove( 'font-dark' );
+        cards.classList.remove('cards-dark')
     }
 
 } )
+
