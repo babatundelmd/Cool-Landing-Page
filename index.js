0fbd4bae4c4cfdfa22c12f347a376a78f3fd272a
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-04-15 23:29:32 +0100

    💄 looks good for now

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const toggle = document.querySelector( 'input[name=check-box]' );
 const body = document.getElementById( 'body' );
+const slider = document.getElementById( 'slider' );
 const container = document.querySelector( '.container' );
-const hone = document.querySelector( 'h1' );
+const hone = document.querySelectorAll( 'div h1, div h2' );
 const cards = document.querySelectorAll( '.cards, .overview' );
+const modeText = document.querySelector( '.mode-text' )
 
 function toggleCard ()
 {
-    cards.forEach( (e) =>
+    cards.forEach( ( e ) =>
     {
-        console.log(e)
         e.classList.toggle( 'cards-dark' );
-    })
+    } );
+
+    hone.forEach( ( e ) =>
+    {
+        e.classList.toggle( 'font-dark' )
+    } )
 }
 
 toggle.addEventListener( 'change', ( e ) =>
@@ -18,15 +24,16 @@ toggle.addEventListener( 'change', ( e ) =>
     if ( e.target.checked === false ) {
         body.classList.toggle( 'dark-theme' );
         container.classList.toggle( 'container-dark' );
-        hone.classList.toggle( 'font-dark' );
+        slider.classList.add( '_slider' );
+        modeText.textContent = 'Light Theme'
         toggleCard();
-        console.log( e, "Dark Theme")
     } else {
-        body.classList.remove( 'dark-theme');
+        body.classList.remove( 'dark-theme' );
         container.classList.remove( 'container-dark' );
-        hone.classList.remove( 'font-dark' );
+        slider.classList.remove( '_slider' );
+        modeText.textContent = 'Dark Theme';
         !toggleCard();
-        // cards.classList.remove('cards-dark')
+
     }
 
 } )
