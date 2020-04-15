dc00d8cf7747d789055df1a76c3f0c1196708d76
Babatunde Lamidi <odunayohlamidi@gmail.com>
2020-04-15 22:42:07 +0100

    🔥 all cards dark

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const toggle = document.querySelector( 'input[name=check-box]' );
 const body = document.getElementById( 'body' );
 const container = document.querySelector( '.container' );
 const hone = document.querySelector( 'h1' );
-const cards = document.querySelector( 'div.cards' );
+const cards = document.querySelectorAll( '.cards, .overview' );
 
-
+function toggleCard ()
+{
+    cards.forEach( (e) =>
+    {
+        console.log(e)
+        e.classList.toggle( 'cards-dark' );
+    })
+}
 
 toggle.addEventListener( 'change', ( e ) =>
 {
@@ -12,13 +19,14 @@ toggle.addEventListener( 'change', ( e ) =>
         body.classList.toggle( 'dark-theme' );
         container.classList.toggle( 'container-dark' );
         hone.classList.toggle( 'font-dark' );
-        cards.classList.toggle( 'cards-dark' );
+        toggleCard();
         console.log( e, "Dark Theme")
     } else {
         body.classList.remove( 'dark-theme');
         container.classList.remove( 'container-dark' );
         hone.classList.remove( 'font-dark' );
-        cards.classList.remove('cards-dark')
+        !toggleCard();
+        // cards.classList.remove('cards-dark')
     }
 
 } )
